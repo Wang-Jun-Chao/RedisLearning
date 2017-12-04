d7a716fd8f739d43be2883488f7497b7b2d2df06
王俊超 <shining-glory@qq.com>
2017-12-04 15:56:33 +0800

    commit

---
+++ 02-Redisson/08-分布式锁和同步器/src/main/java/wjc/redisson/RReadWriteLockDemo.java
@@ -0,0 +1,50 @@
+package wjc.redisson;
+
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
+import org.redisson.api.RedissonClient;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-12-04 15-52
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class RReadWriteLockDemo {
+    public static void main(String[] args) throws InterruptedException {
+        // connects to 127.0.0.1:6379 by default
+        RedissonClient redisson = Redisson.create();
+
+        final RReadWriteLock lock = redisson.getReadWriteLock("lock");
+
+        lock.writeLock().tryLock();
+        System.out.println("main thread gets write lock");
+
+        Thread t = new Thread("sub") {
+            public void run() {
+                RLock r = lock.readLock();
+                r.lock();
+                System.out.println("sub thread get read lock");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                r.unlock();
+            }
+
+            ;
+        };
+
+        t.start();
+        t.join();
+
+        lock.writeLock().unlock();
+
+        t.join();
+
+        redisson.shutdown();
+    }
+}
