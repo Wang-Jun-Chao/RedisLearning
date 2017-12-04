125ce67d46b6ec210f750619c8eaa947ba12c989
王俊超 <shining-glory@qq.com>
2017-12-04 12:14:14 +0800

    commit

---
+++ 02-Redisson/07-分布式集合/src/main/java/wjc/redis/RPriorityDequeDemo.java
@@ -0,0 +1,55 @@
+package wjc.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.redisson.Redisson;
+import org.redisson.api.RPriorityDeque;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-12-04 11-19
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class RPriorityDequeDemo {
+    public static void main(String[] args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        RedissonClient redisson = Redisson.create(config);
+
+        redisson.getKeys().flushall();
+
+        RPriorityDeque<Integer> queue = redisson.getPriorityDeque("anyQueue");
+        queue.trySetComparator(new MyComparator()); // 指定对象比较器
+
+        queue.add(3);
+        queue.add(1);
+        queue.add(2);
+        System.out.println("queue: " + queue);
+        System.out.println("=====");
+
+
+        queue.remove(1);
+        System.out.println("queue: " + queue);
+        System.out.println("=====");
+
+        queue.add(5);
+        System.out.println("queue: " + queue);
+        System.out.println("=====");
+
+        queue.pollFirst();
+        System.out.println("queue: " + queue);
+        System.out.println("=====");
+
+        queue.pollLast();
+        System.out.println("queue: " + queue);
+        System.out.println("=====");
+
+        redisson.getKeys().flushall();
+        redisson.shutdown();
+    }
+}
