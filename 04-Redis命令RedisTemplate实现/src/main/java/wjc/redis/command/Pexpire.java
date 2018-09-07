ee89d9235884841460c777db6c5f4f4ccaf4073b
王俊超 <wangjunchao@gf.com.cn>
2018-09-07 19:09:34 +0800

    commit

---
+++ 04-Redis命令RedisTemplate实现/src/main/java/wjc/redis/command/Pexpire.java
@@ -0,0 +1,37 @@
+package wjc.redis.command;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import wjc.redis.Command;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-09-07 18:51
+ **/
+public class Pexpire extends Command<String, String> {
+    private final static Logger logger = LoggerFactory.getLogger(Pexpire.class);
+
+    @Test
+    public void test() {
+        template.opsForValue().set("mykey", "Hello");
+        RedisSerializer<String> serializer = (RedisSerializer<String>) template.getKeySerializer();
+        template.getConnectionFactory().getConnection().keyCommands().pExpire(
+                serializer.serialize("mykey"), 1400);
+
+        // 四舍五入
+        Long ttl = template.getConnectionFactory().getConnection().keyCommands().ttl(
+                serializer.serialize("mykey"));
+        System.out.println(ttl);
+        Assert.assertEquals(Long.valueOf(1), ttl);
+
+        ttl = template.getConnectionFactory().getConnection().keyCommands().pTtl(
+                serializer.serialize("mykey"));
+        System.out.println(ttl);
+        Assert.assertTrue(Long.valueOf(1400).compareTo(ttl) > 0);
+    }
+}
