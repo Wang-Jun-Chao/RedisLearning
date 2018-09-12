217a8995adc60a0fa8ba35bb91f42cb0c2fd0fc9
王俊超 <wangjunchao@gf.com.cn>
2018-09-12 15:51:52 +0800

    commit

---
+++ 04-Redis命令RedisTemplate实现/src/main/java/wjc/redis/command/lists/BRPop.java
@@ -0,0 +1,49 @@
+package wjc.redis.command.lists;
+
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.util.CollectionUtils;
+import wjc.redis.Command;
+import wjc.redis.util.StringUtils;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-09-12 13:50
+ **/
+public class BRPop extends Command<String, String> {
+    private final static Logger logger = LoggerFactory.getLogger(BRPop.class);
+
+    @Test
+    @Override
+    public void testTemplate() {
+        template.delete(Lists.newArrayList("job", "command", "request"));
+        template.opsForList().leftPush("command", "update system...");
+        template.opsForList().leftPush("request", "visit page");
+
+        Assert.fail("RedisTemplate dose not have brpop command");
+    }
+
+    @Test
+    @Override
+    public void testConnection() {
+        template.opsForList().rightPush("course", "algorithm001");
+        template.opsForList().rightPush("course", "c++101");
+
+        List<byte[]> values = connection.bRPop(3, keySerializer.serialize("course"));
+        List<String> stringValue = Lists.newArrayList();
+        values.forEach(item -> stringValue.add(valueSerializer.deserialize(item)));
+
+        System.out.println(stringValue);
+
+        List<String> set = Lists.newArrayList("course", "c++101");
+
+        Assert.assertTrue(set.containsAll(stringValue) && stringValue.containsAll(set));
+    }
+}
---
+++ 04-Redis命令RedisTemplate实现/src/main/java/wjc/redis/command/lists/BRPopLPush.java
@@ -0,0 +1,60 @@
+package wjc.redis.command.lists;
+
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import wjc.redis.Command;
+import wjc.redis.util.StringUtils;
+
+import java.util.List;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-09-12 13:50
+ **/
+public class BRPopLPush extends Command<String, String> {
+    private final static Logger logger = LoggerFactory.getLogger(BRPopLPush.class);
+
+    @Test
+    @Override
+    public void testTemplate() {
+        template.delete(Lists.newArrayList("job", "command", "request"));
+        template.opsForList().leftPush("command", "update system...");
+        template.opsForList().leftPush("request", "visit page");
+
+        Assert.fail("RedisTemplate dose not have brpoplpush command");
+    }
+
+    @Test
+    @Override
+    public void testConnection() {
+        template.opsForList().rightPush("msg", "hello moto");
+
+
+        byte[] value = connection.bRPopLPush(3, keySerializer.serialize("msg"),
+                keySerializer.serialize("receiver"));
+        System.out.println(valueSerializer.deserialize(value));
+        Assert.assertEquals("hello moto", valueSerializer.deserialize(value));
+
+        Long size = template.opsForList().size("receiver");
+        System.out.println(size);
+        Assert.assertEquals(Long.valueOf(1), size);
+
+        List<byte[]> bytes = connection.lRange(keySerializer.serialize("receiver"), 0, 0);
+        String s = StringUtils.toString(bytes, valueSerializer);
+        System.out.println(s);
+    }
+
+    @Test
+    public void testConnection2() {
+        byte[] value = connection.bRPopLPush(3, keySerializer.serialize("msg"),
+                keySerializer.serialize("receiver"));
+        System.out.println(valueSerializer.deserialize(value));
+        Assert.assertEquals(null, valueSerializer.deserialize(value));
+
+        List<String> list = template.opsForList().range("receiver", 0, 0);
+        System.out.println(list);
+    }
+}
