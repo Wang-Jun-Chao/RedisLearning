acb735a77c7d4859d4c85c30ed699d0e2da4da4c
王俊超 <shining-glory@qq.com>
2018-09-14 05:44:59 +0800

    commit

--- 04-Redis命令RedisTemplate实现/src/main/java/wjc/redis/command/sets/SAdd.java
+++ 04-Redis命令RedisTemplate实现/src/main/java/wjc/redis/command/sets/SAdd.java
@@ -1,6 +1,11 @@
 package wjc.redis.command.sets;
 
+import com.google.common.collect.Sets;
+import org.junit.Assert;
+import org.junit.Test;
 import wjc.redis.Command;
+
+import java.util.Set;
 
 /**
  * <pre>
@@ -13,13 +18,27 @@ import wjc.redis.Command;
  * All Rights Reserved !!!
  */
 public class SAdd extends Command<String, String> {
+    @Test
     @Override
     public void testTemplate() {
-
+        template.opsForSet().add("myset", "Hello", "World", "World");
+        Set<String> members = template.opsForSet().members("myset");
+        Set<String> values = Sets.newHashSet("Hello", "World");
+        System.out.println(values);
+        Assert.assertTrue(members.containsAll(values));
+        Assert.assertTrue(values.containsAll(members));
     }
 
+    @Test
     @Override
     public void testConnection() {
-
+        template.opsForSet().add("myset", "Hello", "World", "World");
+        Set<byte[]> byteMembers = connection.sMembers(keySerializer.serialize("myset"));
+        Set<String> members = Sets.newHashSet();
+        byteMembers.forEach(item -> members.add(valueSerializer.deserialize(item)));
+        Set<String> values = Sets.newHashSet("Hello", "World");
+        System.out.println(values);
+        Assert.assertTrue(members.containsAll(values));
+        Assert.assertTrue(values.containsAll(members));
     }
 }
