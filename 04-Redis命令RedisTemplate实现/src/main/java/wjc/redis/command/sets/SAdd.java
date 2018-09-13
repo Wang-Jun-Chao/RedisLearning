3031f4ffd095908751b02f6f42066bf2b4fcfd75
王俊超 <shining-glory@qq.com>
2018-09-13 22:51:55 +0800

    commit

---
+++ 04-Redis命令RedisTemplate实现/src/main/java/wjc/redis/command/sets/SAdd.java
@@ -0,0 +1,25 @@
+package wjc.redis.command.sets;
+
+import wjc.redis.Command;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ * Author: 王俊超
+ * Date: 2018-09-13 22:51
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class SAdd extends Command<String, String> {
+    @Override
+    public void testTemplate() {
+
+    }
+
+    @Override
+    public void testConnection() {
+
+    }
+}
