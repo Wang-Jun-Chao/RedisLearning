1d6fba3f21cc7b68da4f000edf37caacdd5f10d5
王俊超 <shining-glory@qq.com>
2018-09-16 09:02:17 +0800

    commit

---
+++ 04-Redis命令RedisTemplate实现/src/main/java/wjc/redis/command/hll/PFAdd.java
@@ -0,0 +1,49 @@
+package wjc.redis.command.hll;
+
+import org.junit.Assert;
+import org.junit.Test;
+import wjc.redis.Command;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ * Author: 王俊超
+ * Date: 2018-09-16 08:51
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class PFAdd extends Command<String, String> {
+    @Test
+    @Override
+    public void testTemplate() {
+        Long add = template.opsForHyperLogLog().add("hll", "a", "b", "c", "d", "e", "f", "g");
+        System.out.println(add);
+        Assert.assertEquals(Long.valueOf(1), add);
+
+        Long size = template.opsForHyperLogLog().size("hll");
+        System.out.println(size);
+        Assert.assertEquals(Long.valueOf(7), size);
+    }
+
+    @Test
+    @Override
+    public void testConnection() {
+        Long add = connection.pfAdd(keySerializer.serialize("hll"),
+                valueSerializer.serialize("a"),
+                valueSerializer.serialize("b"),
+                valueSerializer.serialize("c"),
+                valueSerializer.serialize("d"),
+                valueSerializer.serialize("e"),
+                valueSerializer.serialize("f"),
+                valueSerializer.serialize("g"));
+
+        System.out.println(add);
+        Assert.assertEquals(Long.valueOf(1), add);
+
+        Long size = template.opsForHyperLogLog().size("hll");
+        System.out.println(size);
+        Assert.assertEquals(Long.valueOf(7), size);
+    }
+}
